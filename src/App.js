1a0aa675cb302723578ec52a1f34031a9e39bd89
Eric Heinmiller <ericheinmiller@Erics-MacBook-Pro-2.local>
2023-05-23 15:36:07 -0700

    yolo

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from "react";
+import Sidebar from  "./Sidebar.js";
+import Main from  "./Main.js";
 import "./App.css";
 
 function App() {
-  return <div>Hi Baby</div>;
+  return (
+    <>
+      <Sidebar />
+      <Main />
+    </>
+  );
 }
 
 export default App;
