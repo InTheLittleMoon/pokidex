b6975b855c5a3a120efe39d1ac1cf2e7ec2974fa
Gabriel Sanchez <g.sanchez1216@gmail.com>
2023-05-23 15:37:25 -0700

    pokemon fetch call

--- src/App.js
+++ src/App.js
@@ -2,6 +2,16 @@ import React from "react";
 import "./App.css";
 
 function App() {
+  async function pokiFetch() {
+    //add number to end of fetch string for individual pokemon
+    fetch("https://pokeapi.co/api/v2/pokemon/")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      });
+  }
+  pokiFetch();
+
   return <div>Hi Baby</div>;
 }
 
