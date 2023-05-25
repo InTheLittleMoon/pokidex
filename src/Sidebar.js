11cfda6442a629cb15360b688783a9e194a88477
Gabriel Sanchez <124187943+InTheLittleMoon@users.noreply.github.com>
2023-05-24 22:39:29 -0700

    sidebar work

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,25 +1,37 @@
 import React from "react";
 import "./Sidebar.css";
 
+//image imports
+import magnifyingGlassIcon from "./assets/images/sidebar-searchbar-icon.png";
+
 //components
 import FilterOption from "./components/Sidebar-Filter-Options/Sidebar-Filter-Options";
 
 export default function Sidebar() {
   return (
     <div className="sidebar-container">
-      <div className="sidebar-searchbar-container"></div>
+      <div className="sidebar-searchbar-container">
+        <h1>
+          What Pokémon
+          <br /> are you looking for?
+        </h1>
+        <div className="searchbar">
+          <img alt="search" src={magnifyingGlassIcon}></img>
+          <input placeholder="Search Pokémon, Moves, Abilities, etc"></input>
+        </div>
+      </div>
       <div className="sidebar-filters-container">
         <div className="sidebar-filter-row">
-          <FilterOption color={"green"} name={"Pokedex"} />
-          <FilterOption color={"red"} name={"Moves"} />
+          <FilterOption color={0} name={"Pokedex"} />
+          <FilterOption color={1} name={"Moves"} />
         </div>
         <div className="sidebar-filter-row">
-          <FilterOption color={"blue"} name={"Abilities"} />
-          <FilterOption color={"yellow"} name={"Items"} />
+          <FilterOption color={2} name={"Abilities"} />
+          <FilterOption color={3} name={"Items"} />
         </div>
         <div className="sidebar-filter-row">
-          <FilterOption color={"purple"} name={"Locations"} />
-          <FilterOption color={"tan"} name={"Type Charts"} />
+          <FilterOption color={4} name={"Locations"} />
+          <FilterOption color={5} name={"Type Charts"} />
         </div>
       </div>
     </div>
--- src/components/Sidebar-Filter-Options/Sidebar-Filter-Options.js
+++ src/components/Sidebar-Filter-Options/Sidebar-Filter-Options.js
@@ -1,6 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./Sidebar-Filter-Options.css";
 
 export default (props) => {
-  return <h2 className="sidebar-filter-option">{props.name}</h2>;
+  const filterContainerColors = [
+    "#2bdab1",
+    "#f7776a",
+    "#58abf7",
+    "#ffce4b",
+    "#9e5bba",
+    "#ca817a",
+  ];
+  return (
+    <h2
+      style={{ backgroundColor: filterContainerColors[props.color] }}
+      className="sidebar-filter-option"
+    >
+      {props.name}
+    </h2>
+  );
 };
