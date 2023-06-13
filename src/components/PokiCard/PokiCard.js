c73bfb190b0b6bbd4a69fc0130692380a577dc77
Gabriel Sanchez <g.sanchez1216@gmail.com>
2023-06-12 17:09:03 -0700

    fixed card color schemes
    except dark and steel as there exist none with that dominant typing

--- src/components/PokiCard/PokiCard.js
+++ src/components/PokiCard/PokiCard.js
@@ -7,14 +7,18 @@ export default function PokiCard({ pokemon }) {
     <div className={`pokicard-container ${pokemon.types[0].type.name}`}>
       <div className="upper-pokicard">
         <div className="pkName">{pokemon.name}</div>
-        {/* needs to dynamically add font color based off primary typing */}
-        <div className="pkID grassTwo">{pokemon.id}</div>
+        <div className={`pkID ${pokemon.types[0].type.name}Two`}>
+          {pokemon.id}
+        </div>
       </div>
       <div className="lower-pokicard">
         <div className="poki-typings-container">
           {pokemon.types.map((type) => {
-            /* needs to dynamically add background color based off primary typing */
-            return <li className="poki-typing grassThree">{type.type.name}</li>;
+            return (
+              <li className={`poki-typing ${pokemon.types[0].type.name}Three`}>
+                {type.type.name}
+              </li>
+            );
           })}
         </div>
         <div className="poki-image-container">
