3e89f1d18ebfc5bbecde169cc4a84af48729fea4
Eric Heinmiller <ericheinmiller@gmail.com>
2023-06-16 21:03:51 -0700

    changes

--- src/components/Modal/Mark.js
+++ src/components/Modal/Mark.js
@@ -1,7 +1,10 @@
 import React from "react";
 
 export default function Mark({ status }) {
+  console.log("status", status);
   if (status === false) {
     return <div className="mark" />;
+  } else if (status === true) {
+    return <div className="mark-green" />;
   }
 }
--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Mark from "./Mark.js";
 import "./Modal.css";
 
@@ -6,6 +6,43 @@ export default function Modal({ showModal, setShowModal }) {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [passwordConfirm, setPasswordConfirm] = useState("");
+  const [userError, setUserError] = useState(null);
+  const [passwordError, setPasswordError] = useState(null);
+  const [confirmError, setConfirmError] = useState(null);
+
+  useEffect(() => {
+    console.log("password", password);
+    if (password.length && passwordConfirm.length) {
+      if (password !== passwordConfirm) {
+        setPasswordError(false);
+        setConfirmError(false);
+      } else {
+        setPasswordError(null);
+        setConfirmError(null);
+      }
+    }
+  }, [password, passwordConfirm]);
+
+  useEffect(() => {
+    const promise = fetch("http://localhost:8000/checkUsername", {
+      method: "POST",
+      body: JSON.stringify({
+        username,
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    }).then((res) => {
+      return res.json();
+    });
+    promise.then((res) => {
+      if (res) {
+        setUserError(false);
+      } else {
+        setUserError(true);
+      }
+    });
+  }, [username]);
 
   if (!showModal) return null;
   const handleOnClick = (e) => {
@@ -15,19 +52,30 @@ export default function Modal({ showModal, setShowModal }) {
     <div className="overlay" onClick={(e) => handleOnClick(e)}>
       <div className="modal">
         <div>Username</div>
-        <div>
-          <input />
-          <Mark status={null} />
+        <div className="input-container">
+          <input
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <Mark status={userError} />
         </div>
         <div>Password</div>
-        <div>
-          <input type="password" />
-          <Mark status={null} />
+        <div className="input-container">
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <Mark status={passwordError} />
         </div>
         <div>Confirm Password</div>
         <div className="input-container">
-          <input type="password" />
-          <Mark status={null} />
+          <input
+            type="password"
+            value={passwordConfirm}
+            onChange={(e) => setPasswordConfirm(e.target.value)}
+          />
+          <Mark status={confirmError} />
         </div>
         <button className="button">Confirm</button>
       </div>
